122b43cf6d6466ec951f1eb6c1203bbe1984215c
Aloïs Delaune <90683963+AloisD@users.noreply.github.com>
2021-11-02 12:06:50 +0100

    Add files via upload

---
+++ Car.php
@@ -0,0 +1,80 @@
+<?php
+
+require_once 'Vehicle.php';
+
+class Car extends Vehicle
+{
+//////////// properties ///////////
+
+    protected string $energy;
+    protected int $energyLevel;
+    private bool $hasParkBrake = TRUE;
+
+//////////// constants ///////////
+
+    public const ALLOWED_ENERGIES = [
+        'fuel',
+        'electric',
+    ];
+
+//////////// construct //////////////
+
+public function __construct(string $color, int $nbSeats, string $energy)
+{
+    parent::__construct($color, $nbSeats);
+    $this->setEnergy($energy);
+}
+//////////// methods //////////////
+
+    public function setParkBrake():void
+    {
+        $this->hasParkBrake = !$this->hasParkBrake;
+    }
+
+    public function brake(): string
+    {
+        $sentence = "";
+        while ($this->currentSpeed > 0) {
+            $this->currentSpeed = $this->currentSpeed-2;
+            $sentence .= "Brake !!!";
+        }
+        $this->currentSpeed = 0;
+        $sentence .= "I'm stopped !";
+        return $sentence;
+    }
+
+    public function start(): string
+    {
+        if ($this->hasParkBrake==true) {
+            throw new Exception("Veuillez desserrer le frein à main <br>");
+        }
+
+        if (0 == $this->currentSpeed) {
+            $this->currentSpeed = 15; 
+        }
+        return "Go !";
+    }
+
+    public function getEnergy(): string
+    {
+        return $this->energy;
+    }
+
+    public function setEnergy(string $energy): Car
+    {
+        if (in_array($energy, self::ALLOWED_ENERGIES)) {
+            $this->energy = $energy;
+        }
+        return $this;
+    }
+
+    public function getEnergyLevel(): int
+    {
+        return $this->energyLevel;
+    }
+
+    public function setEnergyLevel(int $energyLevel): void
+    {
+        $this->energyLevel = $energyLevel;
+    }
+}
